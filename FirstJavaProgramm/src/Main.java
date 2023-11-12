6edad314a97231c67a2094ce6b6732e4a7afc018
ArtyJava <artemivanovichkulagin@gmail.com>
2023-11-12 17:36:58 +0300

    my first commit

---
+++ FirstJavaProgramm/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Arty";
+        int age = 4213423;
+        System.out.println("Hello world!");
+        System.out.println("My name is " + name);
+        System.out.println("Im " + age + " years old");
+        //System.out.println(age);
+        //System.out.println(name);
+    }
+}
