f87a11f735321111aa57ba105cbee8ffb07b06c5
ArtyJava <artemivanovichkulagin@gmail.com>
2023-11-19 18:28:30 +0300

    new project

---
+++ Something/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        if(a>b){
+            System.out.println("a>b");
+        } else{
+            System.out.println("b>a");
+        }
+    }
+
+}
+
