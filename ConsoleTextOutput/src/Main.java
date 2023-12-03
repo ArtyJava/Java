b830134be8924d75ff136a18d56c8ef25218d1e5
unknown <ArtyJava>
2023-12-03 18:41:35 +0300

    add pine tree

--- ConsoleTextOutput/src/Main.java
+++ ConsoleTextOutput/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // task 1
+        System.out.println("Hello \nHello");
+        // task 2
+        for (int i = 0; i < 10; i++) {
+            String stars = "";
+            for (int j = 0; j <= i; j++) {
+                stars += "*";
+            }
+            System.out.println(stars);
+        }
     }
 }
