cba45552975d78785bdf8c8c29c099917e681af9
unknown <ArtyJava>
2023-12-10 16:38:50 +0300

    TaskAboutCookie

---
+++ VariableTypes/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        byte maloPechenek = 1       ;
+        short chutChutPechenek = 2;
+        int nemnogoPechenek = 4;
+        long ochenMnogoPechenek = 8;
+        float pechenkaAmount = 1.5f;
+        double pechenkaAmount1 = 1.5;
+        boolean hochuPechenki = true;
+        String essayProPechenki = "Oni vkusnie";
+        char pervayaBukvaSlovaPechnka = 'P';
+        System.out.println("maloPechenek = " + maloPechenek);
+        System.out.println("chutChutPechenek = " + chutChutPechenek);
+        System.out.println("nemnogoPechenek = " + nemnogoPechenek);
+        System.out.println("ochenMnogoPechenek = " + ochenMnogoPechenek);
+        System.out.println("pechenkaAmount = " + pechenkaAmount);
+        System.out.println("pechenkaAmount1 = " + pechenkaAmount1);
+        System.out.println("hochuPechenki = " + hochuPechenki);
+        System.out.println("essayProPechenki = " + essayProPechenki);
+        System.out.println("pervayaBukvaSlovaPechnka = " + pervayaBukvaSlovaPechnka);
+
+    }
+}
