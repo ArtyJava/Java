7824b8592f39b0d74db78e5f6c272b629e586625
unknown <ArtyJava>
2023-12-10 16:21:43 +0300

    add task

---
+++ CreateVariable/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Создайте целочисленную переменную с наименьшим диапазоном чисел." +
+                " Укажите для нее значение равное восьми.");
+        byte num1 = 8;
+        System.out.println(num1);
+    }
+}
