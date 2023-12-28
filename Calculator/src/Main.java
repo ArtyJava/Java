59cff81140f150fc552d6a704296ece366434e53
unknown <ArtyJava>
2023-12-28 21:58:43 +0300

    calculator

---
+++ Calculator/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        // 1. request 1st number
+        // 2. re quest 2nd number
+        // 3. request operation
+        // 4. determine result using switch
+        // 5. output result
+
+
+    }
+
+}
