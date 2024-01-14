1128335149deda0a306ce4fca8197c836a581d24
unknown <ArtyJava>
2024-01-14 15:39:10 +0300

    Calculator

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
+        double res = 0;
         // 1. request 1st number
-        // 2. re quest 2nd number
+        System.out.println("Введите первое значение: ");
+        int num1 = new Scanner(System.in).nextInt();
+        // 2. request 2nd number
+        System.out.println("Введите второе значение: ");
+        int num2 = new Scanner(System.in).nextInt();
         // 3. request operation
+        System.out.println("Введите вариат операции: ");
+        String operation = new Scanner(System.in).nextLine();
         // 4. determine result using switch
+        switch (operation) {
+            case  ("+"):
+                res = num1 + num2;
+                break;
+            case ("-"):
+                res = num1 - num2;
+                break;
+            case ("*"):
+                res = num1 * num2;
+                break;
+            case ("/"):
+                res = num1 / num2;
+                break;
+            case ("%"):
+                res = num1 % num2;
+                break;
+            default:
+                System.out.println("Такой операции не существует");
+                break;
+        }
         // 5. output result
+        System.out.println("Результат выполнения операции: " + res);
 
 
     }
